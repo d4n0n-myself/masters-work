ab718b06832c9bfe211c3bb5109d486da744abf6
d4n0n_myself <danon.sibaev@gmail.com>
2024-02-11 17:52:18 +0300

    add tpot, train_test_split

--- autogluon_pca.py
+++ autogluon_pca.py
@@ -2,30 +2,29 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 from autogluon.tabular import TabularPredictor
 
 # Загрузка датасета Iris
 iris = load_iris()
-X = iris.data
-y = iris.target
+# Разделение данных на обучающий и тестовый наборы
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, train_size=0.75, test_size=0.25)
 
-# Применение PCA
+# Применение PCA на обучающем наборе
 pca = PCA(n_components=2)
-X_pca = pca.fit_transform(X)
+X_train_pca = pca.fit_transform(X_train)
 
 # Создание нового датасета с использованием новых признаков
-new_data = np.concatenate((X_pca, y.reshape(-1, 1)), axis=1)
+new_data = np.concatenate((X_train_pca, y_train.reshape(-1, 1)), axis=1)
 new_data = pd.DataFrame(new_data, columns=['feature1', 'feature2', 'target'])
 
 # Создание и обучение модели с помощью AutoGluon
 predictor = TabularPredictor(label='target').fit(new_data, presets='medium_quality')
-# predictor = TabularPredictor.load("AutogluonModels/ag-20240209_065058")
 
 # Прогнозирование на новых данных
-new_X = np.array([[5.1, 3.5, 1.4, 0.2], [5.9, 3.0, 5.1, 1.8], [5.1, 2.5, 3.0, 1.1]])  # Пример новых данных
-new_X_pca = pca.transform(new_X)
-new_data = pd.DataFrame(np.concatenate((new_X_pca, [[0], [2], [1]]), axis=1),
-                        columns=['feature1', 'feature2', 'target'])
-predictions = predictor.predict(new_data)
+X_test_pca = pca.transform(X_test)
+new_data_test = pd.DataFrame(np.concatenate((X_test_pca, y_test.reshape(-1, 1)), axis=1),
+                             columns=['feature1', 'feature2', 'target'])
+predictions = predictor.predict(new_data_test)
 
 print(predictions)
--- tpot_pca.py
+++ tpot_pca.py
@@ -2,14 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
-from pycaret.classification import *
 from sklearn.model_selection import train_test_split
+from tpot import TPOTClassifier
 
 # Загрузка датасета Iris
 iris = load_iris()
-# Разделение данных на обучающий и тестовый наборы
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, train_size=0.75, test_size=0.25)
-
 
 # Применение PCA
 pca = PCA(n_components=2)
@@ -19,14 +17,20 @@ X_pca = pca.fit_transform(X_train)
 new_data = np.concatenate((X_pca, y_train.reshape(-1, 1)), axis=1)
 new_data = pd.DataFrame(new_data, columns=['feature1', 'feature2', 'target'])
 
-# Создание и обучение модели с помощью PyCaret
-clf = setup(data=new_data, target='target')
-best_model = compare_models()
+# Создание и обучение модели с помощью TPOT
+tpot = TPOTClassifier(generations=5, population_size=20, verbosity=2)
+tpot.fit(X_train, y_train)
 
 # Прогнозирование на новых данных
-new_X_pca = pca.transform(X_test)
-new_data = pd.DataFrame(np.concatenate((new_X_pca, y_test.reshape(-1, 1)), axis=1),
-                        columns=['feature1', 'feature2', 'target'])
-predictions = predict_model(best_model, data=new_data)
+predictions = tpot.score(X_test, y_test)
 
+print("fitted pipelines")
+print(tpot.pareto_front_fitted_pipelines_)
+
+print("models:")
+# Вывод использованных моделей
+for model in tpot.fitted_pipeline_:
+    print(model)
+
+print("predictions")
 print(predictions)
